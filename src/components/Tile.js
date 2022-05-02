0bba21f49ee49ef79050cb0d98377e69fff4d3be
Myra Luetke <16mfl1@caslab.queensu.ca>
2022-05-02 15:15:15 -0400

    shows mine image on button click

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -3,9 +3,32 @@ import './Tile.css';
 import mine from './mine.jpg';
 
 class Tile extends React.Component {
+
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            imgSrc: null,
+            imgAlt: ''
+        };
+    }
+
+
+    showMine() {
+        this.setState({
+            imgSrc: mine,
+            imgAlt: "mine"
+        });
+    }
+    
+
     render() {
         return (
-            <button className='tile'><img src={mine} alt='mine'/></button>
+            <button 
+                className="tile" 
+                onClick={() => this.setState({imgSrc: mine, imgAlt: "mine"})}>
+                    <img src={this.state.imgSrc} alt={this.state.imgAlt} />
+            </button>
         );
     }
 }
